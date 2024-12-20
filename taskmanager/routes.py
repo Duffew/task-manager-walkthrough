d2d26c0188a86202b203f547a443fbd78c8c5959
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-12-20 08:36:35 +0000

    completed viewing tasks video

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,7 +5,8 @@ from taskmanager.models import Category, Task
 # create a basic app route to get the app running...
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    tasks = list(Task.query.order_by(Task.id).all())
+    return render_template("tasks.html", tasks=tasks)
 
 
 # create an app route for categories.html
